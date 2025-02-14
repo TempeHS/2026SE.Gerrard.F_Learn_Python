f2217fe47b44de0893160ced6c54dd7e2d5a9799
gerrardfong <gerrard.fong@education.nsw.gov.au>
2025-02-14 01:37:37 +0000

    Cleaned and organised

---
+++ 1-Conditionals/bank/bank.py
@@ -0,0 +1,12 @@
+def main():
+    x = question(input("What do you need? ")).
+
+
+def question(x):
+    replace = x.find("H")
+    if replace == True:
+        print("No money")
+    else:
+        print("$100")
+
+main()
