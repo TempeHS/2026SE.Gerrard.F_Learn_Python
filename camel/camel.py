fc0aa510058e843b4e7078ad31ae886ae8820dd8
gerrardfong <gerrard.fong@education.nsw.gov.au>
2025-02-25 02:16:11 +0000

    camel.py

--- camel/camel.py
+++ camel/camel.py
@@ -1,12 +1,15 @@
 def main():
     words = input("Say something: ").strip()
-    final = convert(words)
+    print(convert(words).lstrip("_"))
+    
+        
+        
+    
 
 def convert(words):
     for letter in words:
         if letter.isupper():
-            
-    
-
+            words = words.replace(letter, "_" + letter.lower())
+    return(words)
 
 main()
