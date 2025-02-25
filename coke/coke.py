bf6e0480b2a6bef74d4afde846dce282be5c9f45
gerrardfong <gerrard.fong@education.nsw.gov.au>
2025-02-25 01:55:10 +0000

    coke.py

--- coke/coke.py
+++ coke/coke.py
@@ -1,24 +1,25 @@
+
 def main():
-    
-    input("Please insert a coin: ")
-    
-    while True:
-        if question_1() == "Yes":
-            break
-        else: 
-            question()
-            
-def question_1():
-    question1 = input("Are you finished? ").capitalize()
-    return question1
+    total = 0
+    while total < 50:
+        x = convert(question())
+        if x not in [25, 10, 5]:
+            print("Not a valid coin, please insert another.")
+        else:
+            total += x
+            if total < 50:
+                print("Please insert more coins, you current total is", total)
+            elif total >= 50:
+                print(f"Thank you for your purchase, your change is {total - 50} cents.")
+
 
 def question():
-    question = input("Please insert a coin: ").capitalize()
+    question = input("Please insert a coin: ").strip()
     return question
 
-            
-            
+def convert(x):
+    return int(x)
 
-Boolean = {"No": "False", "Yes": "True"}
-        
 main()
+
+#Used co-pilot to reorganise the order of my code
